e654f9254cc4470e38c2a8208303f5028eb25200
meghanicky12 <meghacyber1216@gmail.com>
2026-08-16 10:06:52 +0530

    Add files via upload

---
+++ password.123.py
@@ -0,0 +1,6 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+====== RESTART: C:/Users/prahl/AppData/Local/Programs/Python/Python314/Password.py =====
+Enter your password: meghava@1234
+Password Strength: Medium
